53e7535063d8ba6e0b44e7dbd3091481d63c0154
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-10-10 09:51:13 -0600

    added some more styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import "./App.css";
 import CardHolder from "./components/CardHolder"
 import ProjectContainer from "./components/ProjectContainer"
